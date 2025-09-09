bb13cdf952aef355b7a238c6c330ccbd955759a9
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2025-09-10 00:00:50 +0300

    set up register page (no backend for sign in or up yet)

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 #imports
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
-#main app
+#app creation
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 #main route that sends the user to home
 @app.route('/')
 def index():
     return redirect(url_for("home"))
+
+#register route
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    #check if the user submitted the form
+    if request.method == "POST":
+        #if there is no username in the form then it's a sign in request
+        if not request.form.get("username"):
+            email = request.form.get("email")
+            password = request.form.get("password")
+            return redirect(url_for("home"))
+        
+        #if there is a username in the form then it's a sign up request
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        return redirect(url_for("home"))
+    
+    return render_template("register.html")
+
 
 #hoem route
 @app.route('/home')
